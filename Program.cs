61c6cb622411f6a7de1f3a06fb9d04e1dddb2e22
Dan Fuhr <fuhrdan@gmail.com>
2026-08-24 07:28:39 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//** This is a solution
+//** Much like alcohol
+//** It hurts the head
+
+int stoneGameVIII(int* stones, int stonesSize)
+{
+    long long prefix = 0;
+    long long* prefixSum = malloc(sizeof(long long) * stonesSize);
+
+    for (int i = 0; i < stonesSize; i++)
+    {
+        prefix += stones[i];
+        prefixSum[i] = prefix;
+    }
+
+    long long best = prefixSum[stonesSize - 1];
+
+    for (int i = stonesSize - 2; i >= 1; i--)
+    {
+        long long current = prefixSum[i] - best;
+
+        if (current > best)
+        {
+            best = current;
+        }
+    }
+
+    free(prefixSum);
+
+    return (int)best;
+}
